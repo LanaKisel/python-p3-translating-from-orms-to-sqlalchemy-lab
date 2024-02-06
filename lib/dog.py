10caaf4cc23bbbde0d42331ccd375f6f3dfd740a
Lana Kisel <svetlana.kisel.ca@gmail.com>
2024-02-05 18:07:01 -0800

    sqlalchemy lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,29 @@
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
+    session.add(dog)
+    session.commit()
     pass
 
-def get_all(session):
-    pass
+def get_all(session):    
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
+    return session.query(Dog).filter(Dog.id == id).first()
     pass
 
 def find_by_name_and_breed(session, name, breed):
+    return session.query(Dog).filter(Dog.name == name and Dog.breed == breed).first()
     pass
 
 def update_breed(session, dog, breed):
+    session.query(Dog).update({
+        Dog.breed: breed
+    })
     pass
